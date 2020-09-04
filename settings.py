9f967991bfa9406b2252e5a80dbf66aaa3542f26
Kyubum Moon <moonx190@umn.edu>
2020-09-04 18:33:32 +0900

    Introduction Page Modified

--- settings.py
+++ settings.py
@@ -10,12 +10,12 @@ SESSION_CONFIG_DEFAULTS = dict(
 )
 
 SESSION_CONFIGS = [
-    # dict(
-    #    name='public_goods',
-    #    display_name="Public Goods",
-    #    num_demo_participants=3,
-    #    app_sequence=['public_goods', 'payment_info']
-    # ),
+    dict(
+       name='introduction',
+       display_name="Introduction to Behavioral Experiment",
+       num_demo_participants=1,
+       app_sequence=['introduction']
+    ),
 ]
 
 
