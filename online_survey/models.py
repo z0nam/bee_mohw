3d5baf43b1d05c1e6b9e0463c5595c56a1a2c466
Kyubum Moon <moonx190@umn.edu>
2020-09-16 14:00:11 +0900

    online survey chapter six added

--- online_survey/models.py
+++ online_survey/models.py
@@ -10,7 +10,6 @@ from otree.api import (
 )
 from Global_Constants import GlobalConstants
 
-
 author = 'Kyubum Moon<mailto:moonx190@umn.edu>'
 
 doc = """
@@ -34,6 +33,7 @@ class Constants(BaseConstants):
         [FREELANCER, "③프리랜서"],
         [UNPAID, "④최근 6개월 이내 근무경력 없음"]
     ]
+
 
 class Subsession(BaseSubsession):
     pass
@@ -71,7 +71,7 @@ class Player(BasePlayer):
             [1, "(2) 예. 5갑(100개비) 이상"],
             [9999, "(3) 아니오. 피운 적 없음"],
         ],
-    widget = widgets.RadioSelect,
+        widget=widgets.RadioSelect,
     )
     within_past_one_year_smoking_cessation_attempted = models.IntegerField(
         label="최근 1년 동안 담배를 끊고자 하루(24시간) 이상 금연한 적이 있습니까?",
@@ -161,7 +161,7 @@ class Player(BasePlayer):
 
     occupation_op = models.StringField(
         label="기타(직접입력)",
-        blank = True,
+        blank=True,
     )
 
     firm_type = models.IntegerField(
@@ -180,7 +180,7 @@ class Player(BasePlayer):
 
     firm_type_op = models.StringField(
         label="기타(위에 해당하지 않는 경우 자유 입력)",
-        blank = True,
+        blank=True,
     )
 
     firm_size = models.IntegerField(
@@ -391,12 +391,12 @@ class Player(BasePlayer):
 
     overall_health_evaluation = models.IntegerField(
         label="귀하가 생각하는 본인의 현재 건강상태는 어떻습니까?",
-        choices=range(0,11),
+        choices=range(0, 11),
         blank=True,
     )
     overall_stress_evaluation = models.IntegerField(
         label="평소 귀하의 스트레스 정도는 어떻습니까?",
-        choices=range(0,11),
+        choices=range(0, 11),
         blank=True,
     )
 
@@ -650,3 +650,320 @@ class Player(BasePlayer):
         ],
         widget=widgets.RadioSelectHorizontal,
     )
+
+    do_you_use_liquid_cigarette = models.IntegerField(
+        label="귀하께서는 현재 액상형 전자담배(쥴, 탱크형 등)를 사용하십니까?",
+        choices=[
+            [1, "매일 사용"],
+            [2, "가끔 사용"],
+            [3, "과거엔 사용했으나, 현재는 사용하지 않음"],
+            [4, "과거에도 , 현재에도 사용한 적 없음"],
+        ],
+        widget=widgets.RadioSelect,
+    )
+
+    liquid_cigarette_start_year = models.IntegerField(
+        label="",
+        choices=[
+            [1, "00"],
+            [2, "01"],
+            [3, "02"],
+            [4, "03"],
+            [5, "04"],
+            [6, "05"],
+            [7, "06"],
+            [8, "07"],
+            [9, "08"],
+            [10, "09"],
+            [11, "10"],
+            [12, "11"],
+            [13, "12"],
+            [14, "13"],
+            [15, "14"],
+            [16, "15"],
+            [17, "16"],
+            [18, "17"],
+            [19, "18"],
+            [20, "19"],
+            [21, "20"],
+        ],
+        blank=True,
+    )
+
+    liquid_cigarette_start_month = models.IntegerField(
+        label="",
+        choices=range(1,13),
+        blank=True,
+    )
+
+    liquid_cigarette_end_year = models.IntegerField(
+        label="",
+        choices=[
+            [1, "00"],
+            [2, "01"],
+            [3, "02"],
+            [4, "03"],
+            [5, "04"],
+            [6, "05"],
+            [7, "06"],
+            [8, "07"],
+            [9, "08"],
+            [10, "09"],
+            [11, "10"],
+            [12, "11"],
+            [13, "12"],
+            [14, "13"],
+            [15, "14"],
+            [16, "15"],
+            [17, "16"],
+            [18, "17"],
+            [19, "18"],
+            [20, "19"],
+            [21, "20"],
+        ],
+        blank=True,
+    )
+    liquid_cigarette_end_month = models.IntegerField(
+        label="",
+        choices=range(1,13),
+        blank=True,
+    )
+
+    most_recent_month_liquid_cigarette_use_days = models.IntegerField(
+        label="최근 1달 간 액상형 전자담배(쥴, 탱크형 등) 사용일수",
+        choices=range(1, 31),
+        blank=True,
+    )
+
+    liquid_amount_of_liquid_cigarette_recent_week = models.IntegerField(
+        label="",
+        choices=range(101),
+        blank=True,
+    )
+
+    liquid_cigarette_density = models.IntegerField(
+        label="귀하가 현재 사용하는 또는 과거에 사용한 액상형 전자담배(쥴, 탱크형 등) 액상의 니코틴 농도(액상 1ml 기준 니코틴 함유량)는 얼마입니까? (1mg/ml=0.1%)",
+        choices=[
+            [1, "니코틴 없음"],
+            [2, "1~6mg/ml"],
+            [3, "7~12mg/ml"],
+            [4, "13~18mg/ml"],
+            [5, "19mg/ml 이상"],
+            [6, "잘 모름"],
+        ],
+        widget=widgets.RadioSelect,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_1 = models.BooleanField(
+        label="아침에 일어나자마자",
+        widget=widgets.CheckboxInput,
+        blank=True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_2 = models.BooleanField(
+        label="잠들기 전",
+        widget=widgets.CheckboxInput,
+        blank=True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_3 = models.BooleanField(
+        label="식사 후",
+        widget=widgets.CheckboxInput,
+        blank=True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_4 = models.BooleanField(
+        label="화장실에서/샤워 후",
+        widget=widgets.CheckboxInput,
+        blank=True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_5 = models.BooleanField(
+        label="휴식시간",
+        widget=widgets.CheckboxInput,
+        blank=True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_6 = models.BooleanField(
+        label="습관적 상황에서 (활력이 필요할 때/전자담배를 사용하지 않음을 깨달을 때, 술/커피마실 때, 혼자 있거나 무언가 기다릴 때등)",
+        widget=widgets.CheckboxInput,
+        blank=True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_7 = models.BooleanField(
+        label = "긍정적 상황 (친구나 가족과 함께 있을 때, 대화나 피로를 풀 때 등)",
+        widget = widgets.CheckboxInput,
+        blank = True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_8 = models.BooleanField(
+        label = "부정적 상황 (스트레스 받을 때, 일이 뜻대로 안될 때, 화날 때 등)",
+        widget = widgets.CheckboxInput,
+        blank = True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_9 = models.BooleanField(
+        label = "흡연자와 같이 있거나, TV의 배우 또는 주위 흡연자의 모습을 보았을 때 ",
+        widget = widgets.CheckboxInput,
+        blank = True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_10 = models.BooleanField(
+        label = "사용을 참기 힘들지 않음",
+        widget = widgets.CheckboxInput,
+        blank = True,
+    )
+
+    liquid_cigarette_hardest_to_resist_when_11 = models.StringField(
+        label = "기타(직접입력:)",
+        blank = True,
+    )
+
+    liquid_cigarette_first_in_the_day = models.IntegerField(
+        label="(과거 액상형 전자담배(쥴, 탱크형 등) 사용자는 사용당시 기준으로 작성) 아침에 일어나서 얼마 만에 첫 액상형 전자담배(쥴, 탱크형 등)를 사용하십니까?",
+        choices=[
+            [1, "(3) 5분 이내"],
+            [2, "(2) 6분~30분 사이"],
+            [3, "(1) 31분~1시간 사이"],
+            [4, "(0) 1시간 이후"],
+        ],
+        widget=widgets.RadioSelect,
+    )
+
+    is_it_hard_to_resist_smoking_liquid_cigarette_in_public = models.BooleanField(
+        label="(과거 액상형 전자담배(쥴, 탱크형 등) 사용자는 사용당시 기준으로 작성) 금연구역(도서관, 극장, 병원 등)에서 액상형 전자담배(쥴, 탱크형 등)를 참기가 어렵습니까?",
+        choices=[
+            [1, "(1) 예"],
+            [2, "(0) 아니오"],
+        ],
+        widget=widgets.RadioSelectHorizontal,
+    )
+
+    when_is_the_liquid_cigarette_the_most_tasty = models.BooleanField(
+        label="(과거 액상형 전자담배(쥴, 탱크형 등) 사용자는 사용당시 기준으로 작성) 하루 중 액상형 전자담배(쥴, 탱크형 등) 맛이 가장 좋은 때는 언제입니까?",
+        choices=[
+            [1, "(1) 아침 첫 사용시기"],
+            [2, "(0) 그 외의 사용시기"],
+        ],
+        widget=widgets.RadioSelectHorizontal,
+    )
+
+    liquid_cigarette_use_frequency_per_day = models.IntegerField(
+        label="(과거 액상형 전자담배(쥴, 탱크형 등) 사용자는 사용당시 기준으로 작성) 하루에 보통 몇 번 사용하십니까?",
+        choices = [
+                  [1, "(0) 10회 이하"],
+                  [2, "(1) 11~20회"],
+                  [3, "(2) 21~30회"],
+                  [4, "(3) 31회 이상"],
+              ],
+        widget = widgets.RadioSelect,
+    )
+
+    liquid_cigarette_morning_is_more_tasty_than_the_rest = models.BooleanField(
+        label="(과거 액상형 전자담배(쥴, 탱크형 등) 사용자는 사용당시 기준으로 작성) 아침에 일어나서 첫 몇 시간 동안 하루 중 다른 시간보다 더 자주 액상형 전자담배(쥴, 탱크형 등)를 사용하십니까?",
+        choices=[
+            [1, "(1) 예"],
+            [2, "(0) 아니오"],
+        ],
+        widget=widgets.RadioSelectHorizontal,
+    )
+
+    liquid_cigarette_sick_all_day_still_smoking = models.BooleanField(
+        label="(과거 액상형 전자담배(쥴, 탱크형 등) 사용자는 사용당시 기준으로 작성) 몸이 아파 하루 종일 누워있을 때에도 액상형 전자담배(쥴, 탱크형 등)를 사용하십니까?",
+        choices=[
+            [1, "(1) 예"],
+            [2, "(0) 아니오"],
+        ],
+        widget=widgets.RadioSelectHorizontal,
+    )
+
+    do_you_use_tobacco_type_e_cigarette = models.IntegerField(
+        label="현재 궐련형 전자담배(가열담배, 예: 아이코스, 글로, 릴 등)를 사용하십니까?",
+        choices=[
+            [1, "매일 사용함"],
+            [2, "가끔 사용함"],
+            [3, "과거엔 사용했었으나, 현재는 사용하지 않음"],
+            [4, "과거에도 , 현재에도 사용한 적 없음"],
+        ],
+        widget=widgets.RadioSelect,
+    )
+
+    tobacco_type_e_cigarette_start_year = models.IntegerField(
+        label="",
+        choices=[
+            [1, "00"],
+            [2, "01"],
+            [3, "02"],
+            [4, "03"],
+            [5, "04"],
+            [6, "05"],
+            [7, "06"],
+            [8, "07"],
+            [9, "08"],
+            [10, "09"],
+            [11, "10"],
+            [12, "11"],
+            [13, "12"],
+            [14, "13"],
+            [15, "14"],
+            [16, "15"],
+            [17, "16"],
+            [18, "17"],
+            [19, "18"],
+            [20, "19"],
+            [21, "20"],
+        ],
+        blank=True,
+    )
+    tobacco_type_e_cigarette_start_month = models.IntegerField(
+        label="",
+        choices=range(1, 13),
+        blank=True,
+    )
+
+    tobacco_type_e_cigarette_end_year = models.IntegerField(
+        label="",
+        choices=[
+            [1, "00"],
+            [2, "01"],
+            [3, "02"],
+            [4, "03"],
+            [5, "04"],
+            [6, "05"],
+            [7, "06"],
+            [8, "07"],
+            [9, "08"],
+            [10, "09"],
+            [11, "10"],
+            [12, "11"],
+            [13, "12"],
+            [14, "13"],
+            [15, "14"],
+            [16, "15"],
+            [17, "16"],
+            [18, "17"],
+            [19, "18"],
+            [20, "19"],
+            [21, "20"],
+        ],
+        blank=True,
+    )
+    tobacco_type_e_cigarette_end_month = models.IntegerField(
+        label="",
+        choices=range(1, 13),
+        blank=True,
+    )
+
+    within_recent_month_days_of_use_for_tobacco_type_e_cigarette = models.IntegerField(
+        label="",
+        choices=range(1, 31),
+        blank=True,
+    )
+
+    tobacco_type_e_cigarette_avg_sticks_per_day = models.IntegerField(
+        label="",
+        choices=range(1, 101),
+        blank=True,
+    )
+
+
